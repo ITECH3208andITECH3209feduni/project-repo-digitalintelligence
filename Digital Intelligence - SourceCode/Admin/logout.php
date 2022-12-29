7393e0a12e62f212e40cd85710d813b50138f325
ddesai18 <84662691+ddesai18@users.noreply.github.com>
2022-12-29 21:30:00 +1100

    Reboot

--- Digital Intelligence - SourceCode/Admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-ob_start();
-error_reporting(0);
-session_start();
-session_destroy();
-
-header('location:index.php');
-?>
--- Digital Intelligence - SourceCode/User/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn=mysqli_connect("localhost","root","","digitalintelligence");
-
-session_start();
-
-session_destroy();
-header("location:index.php");
-
-?>
